348df97020f1737e8b52da0e4a9490d40be5ffab
m4nylovve <149328696+m4nylovve@users.noreply.github.com>
2024-03-13 22:22:55 +0300

    site

---
+++ BURGERS/LOH.js
@@ -0,0 +1,8 @@
+const button = document.querySelector(".navigation_button")
+button.addEventListener('click', () => {
+    alert('В РАЗРАБОТКЕ')
+})
+const button1 = document.querySelector(".make_order")
+button1.addEventListener('click', () => {
+    alert('В РАЗРАБОТКЕ')
+})
--- learning.js
+++
@@ -1,4 +0,0 @@
-let word = prompt().toLowerCase()
-if (word.startsWith('ж')) {
-    console.log(word.length)
-} else {console.log('Попробуйте снова')}
